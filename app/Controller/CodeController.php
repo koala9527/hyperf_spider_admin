9821528a63fe73ae61fe8a395eaa027764c62665
吕振 <1414707572@qq.com>
2020-09-01 18:02:23 +0800

    testmysql

--- app/Controller/CodeController.php
+++ app/Controller/CodeController.php
@@ -4,35 +4,35 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Hyperf\Utils\ApplicationContext;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Annotation\RequestMapping;
 use Hyperf\HttpServer\Annotation\Controller;
-use Hyperf\HttpServer\Annotation\Middleware;
-use App\Middleware\AuthMiddleware;
-use App\JsonRpc\AppServiceInterface;
+//use Hyperf\HttpServer\Annotation\Middleware;
+use Hyperf\DbConnection\Db;
 
 /**
  * @Controller(prefix="/agent")
- * @Middleware(AuthMiddleware::class)
+
  */
 class CodeController extends BaseController
 {
-
     /**
      * 获取列表
      *
      * @RequestMapping(path="getCodeList",methods = "get")
      * 
      */
-public function getCodeList(RequestInterface $request)
-{
-    $name = $request->input('name',null);
-    $one_class = $request->input('one_class',null);
-    $two_class =  $request->input('two_class',null);
-    $page = (int) $request->input('page', 1);
-    $page_size = (int) $request->input('page_size', null);
-    return $this->service->getAgentList($name, $one_class, $two_class, $page, $page_size);
+    public function getCodeList(RequestInterface $request)
+    {
+        $name = $request->input('name',null);
+        $one_class = $request->input('one_class',null);
+        $two_class =  $request->input('two_class',null);
+        $page = (int) $request->input('page', 1);
+        $page_size = (int) $request->input('page_size', null);
+        $data = Db::connection('XCWY')->select('SELECT * FROM code_res limit 1;');
+        return $data;
 
-}
+    }
+
+
 }
--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -44,4 +44,42 @@ return [
             ],
         ],
     ],
+    'XCWY' => [
+        'driver' => env('DB_XCWY_DRIVER', 'mysql'),
+        'read' => [
+            'host' => env('DB_XCWY_READ_HOST') ? explode(',', env('DB_XCWY_READ_HOST')) : [env('DB_XCWY_HOST', 'localhost')],
+        ],
+        'write' => [
+            'host' => [env('DB_XCWY_WRITE_HOST') ?: env('DB_XCWY_HOST', 'localhost')],
+        ],
+        'database' => env('DB_XCWY_DATABASE', 'hyperf'),
+        'username' => env('DB_XCWY_USERNAME', 'root'),
+        'password' => env('DB_XCWY_PASSWORD', ''),
+        'charset' => env('DB_XCWY_CHARSET', 'utf8mb4'),
+        'collation' => env('DB_XCWY_COLLATION', 'utf8mb4_general_ci'),
+        'prefix' => env('DB_XCWY_PREFIX', ''),
+        'pool' => [
+            'min_connections' => 1,
+            'max_connections' => 50,
+            'connect_timeout' => 10.0,
+            'wait_timeout' => 3.0,
+            'heartbeat' => -1,
+            'max_idle_time' => (float) env('DB_XCWY_MAX_IDLE_TIME', 60),
+        ],
+        'cache' => [
+            'handler' => Hyperf\ModelCache\Handler\RedisHandler::class,
+            'cache_key' => 'mc:%s:m:%s:%s:%s',
+            'prefix' => 'default',
+            'ttl' => 3600 * 24,
+            'empty_model_ttl' => 600,
+            'load_script' => true,
+        ],
+        'commands' => [
+            'db:model' => [
+                'path' => 'app/Model',
+                'force_casts' => true,
+                'inheritance' => 'Model',
+            ],
+        ],
+    ],
 ];
